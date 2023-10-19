4a434c141545bd0d262875c27501c6095c5a2f7e
m.ganciauskas <mantukastest@gmail.com>
2023-10-19 13:37:37 +0300

    first commit

--- game.py
+++ game.py
@@ -1,18 +1,18 @@
 # kortu kalade yra listas.
 # korta tai yra stringas susidedantis is 2 simboliu (skaicius ir rusis)
-# hearts, spades, diamonds, clubs from 2 to 10 and J Q K T
+# hearts, spades, diamonds, clubs from 2 to 10 and j q k a
 
 # struktura kaip atskirti kortas
 # sugalvoti kaip paemus viena korta is kalades, kad kaladeje tos kortos neliktu
 
 
-hearts = ["h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "hj", "hq", "hk", "ht"]
+hearts = ["h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "hj", "hq", "hk", "ha"]
 
-spades = ["s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "sj", "sq", "sk", "st"]
+spades = ["s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "sj", "sq", "sk", "sa"]
 
-diamonds = ["d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "dj", "dq", "dk", "dt"]
+diamonds = ["d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "dj", "dq", "dk", "da"]
 
-clubs = ["c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "cj", "cq", "ck", "ct"]
+clubs = ["c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "cj", "cq", "ck", "ca"]
 
 cards = [hearts, spades, diamonds, clubs]
 
