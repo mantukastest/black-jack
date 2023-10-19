bdf846b7181905bb16158a779294ea3a8381282d
m.ganciauskas <mantukastest@gmail.com>
2023-10-19 13:20:46 +0300

    first commit

--- game.py
+++ game.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    print("my first program")
+# kortu kalade yra listas.
+# korta tai yra stringas susidedantis is 2 simboliu (skaicius ir rusis)
+# hearts, spades, diamonds, clubs from 2 to 10 and J Q K T
+
+# struktura kaip atskirti kortas
+# sugalvoti kaip paemus viena korta is kalades, kad kaladeje tos kortos neliktu
+
+
+hearts = ["h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "hj", "hq", "hk", "ht"]
+
+spades = ["s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "sj", "sq", "sk", "st"]
+
+diamonds = ["d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "dj", "dq", "dk", "dt"]
+
+clubs = ["c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "cj", "cq", "ck", "ct"]
+
+cards = [hearts, spades, diamonds, clubs]
+
+print(cards)
+
